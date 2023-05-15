f4b9b44264aa0467f1856a3cd77fc5f496084733
DeepVelvet <49059469+Dormitori@users.noreply.github.com>
2023-05-15 19:09:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,55 @@ import numpy as np
 from numpy import sin, log
 
 
-
+from scipy.integrate import solve_ivp
 from scipy.integrate import odeint
 from scipy.misc import derivative
 
 from tkinter import *
 from sympy import *
+from tkinter import ttk
 
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
 n = -1
+dRx = []
+dRy = []
+dfx = []
+x_cur = [[]]
+num_points = 50
+x_res = []
+t1 = []
+a = 0
+b = 0
+def define_diff():
+    xa = []
+    xb = []
+    x = []
+    for i in range(n):
+        xa.append(Symbol("xa"+str(i + 1)))
+        xb.append(Symbol("xb" + str(i + 1)))
+        x.append(Symbol("x" + str(i + 1)))
+    for i in range(n):
+        tmp1 = []
+        tmp2 = []
+        tmp3 = []
+        for k in range(n):
+            tmp1.append(diff(edge[i], xa[k]))
+            tmp2.append(diff(edge[i], xb[k]))
+            tmp3.append(diff(func[i],x[k]))
+        dRx.append(tmp1)
+        dRy.append(tmp2)
+        dfx.append(tmp3)
 
-def solve_ode(y, t):
+
+def solve_ode(x1, x2):
     plot = Tk()
     fig = Figure(figsize=(5, 5), dpi=100)
     plot1 = fig.add_subplot(111)
-    plot1.plot(t, y)
-
+    plot1.plot(x1, x2)
+    plot1.grid()
     canvas = FigureCanvasTkAgg(fig, master=plot)
     canvas.draw()
 
@@ -72,12 +102,8 @@ def fi0(p0, a, b, t0):
     return res
 
 def dRdxdy(p, a, b, t0):
-    t = np.linspace(t0, a)
-    res_ode = odeint(fun_mat, p, t)
-    x_a_p = res_ode[-1]
-    t = np.linspace(t0, b)
-    res_ode = odeint(fun_mat, p, t)
-    x_b_p = res_ode[-1]
+    x_a_p = x_cur[0]
+    x_b_p = x_cur[-1]
     dRdx = np.zeros((n, n))
     dRdy = np.zeros((n, n)) #laksjdflkajsefjsaeiojfioasjfiojasef
     xa = []
@@ -87,60 +113,114 @@ def dRdxdy(p, a, b, t0):
         xb.append(Symbol("xb" + str(i + 1)))
     for i in range(n):
         for k in range(n):
-            tmp_fun = diff(edge[k], xa[i])
+            tmp_fun = dRx[k][i]
             for j in range(n):
                 tmp_fun = tmp_fun.subs(xa[j], x_a_p[j])
                 tmp_fun = tmp_fun.subs(xb[j], x_b_p[j])
             dRdx[k][i] = tmp_fun.evalf()
     for i in range(n):
         for k in range(n):
-            tmp_fun = diff(edge[k], xb[i])
+            tmp_fun = dRy[k][i]
             for j in range(n):
                 tmp_fun = tmp_fun.subs(xa[j], x_a_p[j])
                 tmp_fun = tmp_fun.subs(xb[j], x_b_p[j])
             dRdy[k][i] = tmp_fun.evalf()
     return [dRdx, dRdy]
 
-def Xrhs(y, tt, p):
+def Xrhs(tt, y):
     t = Symbol("t")
     x = []
+    x_c = x_cur[ind(tt, a, b)]
     for i in range(1, 7):
         x.append(Symbol("x" + str(i)))
     dfdxp = np.zeros((n, n))
     for i in range(n):
         for k in range(n):
-            tmp_fun = diff(func[k], x[i])
+            tmp_fun = dfx[k][i]
             tmp_fun.subs(t, tt)
             for j in range(n):
-                tmp_fun = tmp_fun.subs(x[j], p[j])
+                tmp_fun = tmp_fun.subs(x[j], x_c[j])
             dfdxp[k][i] = tmp_fun.evalf()
     return np.matmul(dfdxp, np.array(y))
 
 def X(p, a, b):
-    rhs = lambda y, tt: Xrhs(y, tt, p)
     res = []
-    t = np.linspace(a, b)
+    t = np.linspace(a, b, num=5)
     for i in range(n):
         init = np.zeros(n)
         init[i] = 1
-        res.append(odeint(rhs, init, t)[-1])
+        res.append(solve_ivp(Xrhs, [a, b], init).y[:,-1])
     return np.array(res).transpose()
 
+
+def ind(t, a, b):
+    res = int(np.floor((t - a) * (num_points - 1) / (b - a)))
+    if (res < 0):
+        res = 0
+    if (res > num_points - 1):
+        res = num_points - 1
+    return res
+
+
 def dfidmu(p, a, b, t0):
+    t = np.linspace(a, b, num=num_points)
+    global x_cur
+    x_cur = odeint(fun_mat, p, np.flip(t[0:(ind(t0, a, b) + 1)]))
+    x_cur = np.flip(x_cur, 0)
+    x_cur = np.append(x_cur, odeint(fun_mat, p, t[ind(t0, a, b):])[1:], axis=0)
+
     R = dRdxdy(p, a, b, t0)
     Xa = X(p, t0, a)
     Xb = X(p, t0, b)
     res = np.matmul(R[0], Xa) + np.matmul(R[1], Xb)
     return res
 
-def prhs(p, mu, a, b, t0, f0):
+def prhs(mu, p, a, b, t0, f0):
+
+    print(p, " ", mu)
+    pb['value'] = mu * 100
+    window.update()
     f_inv = np.linalg.inv(dfidmu(p, a, b, t0))
     return -np.matmul(f_inv, f0)
 
+def test():
+    func.append(parse_expr("x3*x2"))
+    func.append(parse_expr("x3*(-x1+sin(x2))"))
+    func.append(parse_expr("0"))
+    func.append(parse_expr("0"))
+    edge.append(parse_expr("xa1-xa4"))
+    edge.append(parse_expr("xb1-xb4"))
+    edge.append(parse_expr("xa2"))
+    edge.append(parse_expr("xb2"))
+    global a, b, n
+    a = 0
+    b = 1
+    n = 4
+
+
+pb = 0
+wait = 0
+
+def draw():
+    global t1, x_res
+    conv = {
+        "t" : t1
+    }
+    for i in range(n):
+        conv["x" + str(i + 1)] = x_res[:, i]
+    solve_ode(conv[dr1.get()], conv[dr2.get()])
+
+
 def tm():
+    window.update()
+    global a, b
+    global x_res
+    global t1
     global func, edge
     func = []
     edge = []
+    button_draw["state"] = DISABLED
+    window.update()
     for i in range(1, 7):
         input = texts_diff[i - 1].get("1.0", "end-1c")
         if input != "":
@@ -153,23 +233,32 @@ def tm():
     print(n)
     n = len(func)
     print(n)
+
     a = eval(init_a.get("1.0", "end-1c"))
     b = eval(init_b.get("1.0", "end-1c"))
     p0 = np.array(eval(init_value.get("1.0", "end-1c")))
     t0 = eval(init_time.get("1.0", "end-1c"))
+
+
+    #test()
+    #p0 = [2,0,2*3.1415926,2]
+    #t0 = 0
+
+    define_diff()
     f0 = fi0(p0, a, b, t0)
-    rhs = lambda y, tt: prhs(y, tt, a, b, t0, f0)
-    mu = np.linspace(0, 1)
-    p_res = odeint(rhs, p0, mu)[-1]
+    rhs = lambda tt, y: prhs(tt, y, a, b, t0, f0)
+    mu = np.linspace(0, 1, num=3)
+    p_res = solve_ivp(rhs, [0,1], p0, method='RK23').y[:,-1]
     t1 = np.linspace(t0, a)
     x_res = odeint(fun_mat, p_res, t1)
-    x_res = np.flip(x_res)
+    x_res = np.flip(x_res, 0)
     t = np.linspace(t0, b)
     x_res = np.append(x_res, odeint(fun_mat, p_res, t), axis=0)
     t1 = np.flip(t1)
     t1 = np.append(t1, t)
-    print(x_res)
-    solve_ode(x_res, t1)
+    button_draw["state"] = NORMAL
+    window.update()
+
 
 
 window = Tk()
@@ -234,11 +323,34 @@ Label(fr1, text="Вектор начального приближения:", fon
 init_value = Text(fr1, width=10, height=1, font=("Arial", 15))
 init_value.pack(side="left", expand=True, pady=5, padx=40)
 
+fr1 = Frame(fr)
+fr1.pack(side=TOP)
+Label(fr1, text="Оси графиков:", font=font).pack(side="left", expand=True, pady=5, padx=15)
+
+dr1 = ttk.Combobox(
+    fr1,
+    width = 5,
+    state="readonly",
+    values=["t", "x1", "x2", "x3", "x4", "x5", "x6"]
+)
+dr2 = ttk.Combobox(
+    fr1,
+    width = 5,
+    state="readonly",
+    values=["t", "x1", "x2", "x3", "x4", "x5", "x6"]
+)
+dr1.pack(pady=16, side=LEFT)
+dr2.pack(pady=16, side=LEFT)
+button_draw = Button(fr1, text="Нарисовать", command=draw, state=DISABLED)
+button_draw.pack(side=LEFT, fill=BOTH, pady=16, padx=15)
 
 
 fr = Frame(window)
 fr.pack(side=BOTTOM)
-Button(window, text="Выполнить", command=tm).pack(side=BOTTOM,fill=BOTH, pady=16)
+Button(fr, text="Выполнить", command=tm).pack(side=BOTTOM, fill=BOTH, pady=16)
+pb = ttk.Progressbar(window, orient="horizontal", length=200, value=0)
+pb.pack(pady=16, side=BOTTOM)
+
 
 func = []
 edge = []
